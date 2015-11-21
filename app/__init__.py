65d060906a3f1eb8c6ef4b15b4a8c2f904aa1c17
axazeano <vovchik161@gmail.com>
2015-11-22 02:35:22 +0300

    fix blueprint registration

--- app/__init__.py
+++ app/__init__.py
@@ -9,11 +9,7 @@ from flask.ext.mongoengine import MongoEngine
 #     app.config["SECRET_KEY"] = ""
 #     db = MongoEngine().connection
 #
-#     from .main import main as main_blueprint
-#     app.register_blueprint(main_blueprint)
-#
-#     from .survey import survey as survey_blueprint
-#     app.register_blueprint(survey_blueprint,  url_prefix='/survey')
+
 #
 #     return app, db
 #
@@ -22,6 +18,11 @@ from flask.ext.mongoengine import MongoEngine
 app = Flask(__name__)
 app.config["MONGODB_SETTINGS"] = {'DB': "MMCS_survey", 'alias':'default', 'host':'localhost'}
 app.config["SECRET_KEY"] = ""
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
+
+from .survey import survey as survey_blueprint
+app.register_blueprint(survey_blueprint,  url_prefix='/survey')
 db = MongoEngine(app)
 
 
