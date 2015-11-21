416aa118c8f8e4f38cdfcef24a2889d07c56892a
axazeano <vovchik161@gmail.com>
2015-11-21 19:50:35 +0300

    Add mongo instanse

--- MMCS_Survey.py
+++ MMCS_Survey.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from flask import render_template
+from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__)
+
+app.config["MONGODB_SETTINGS"] = {'DB': "my_tumble_log"}
+app.config["SECRET_KEY"] = "KeepThisS3cr3t"
+
+db = MongoEngine(app)
 
 
 @app.route('/')
