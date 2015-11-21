ed6627aacb500c1926b8db468bbf80fd35edfe4c
axazeano <vovchik161@gmail.com>
2015-11-21 19:57:06 +0300

    Add basic model

--- model.py
+++ model.py
@@ -1,3 +1,19 @@
 import datetime
 from flask import url_for
 from MMCS_Survey import db
+
+class BasicSurvey(db.Document):
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+    title = db.StringField(max_length=255, required=True)
+
+    def get_absolute_url(self):
+        return url_for('post', kwargs={"slug": self.slug})
+
+    def __unicode__(self):
+        return self.title
+
+    meta = {
+        'allow_inheritance': True,
+        'indexes': ['-created_at', 'slug'],
+        'ordering': ['-created_at']
+    }
