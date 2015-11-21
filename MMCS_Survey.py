f58a4f6edabda50bb24b5af9336b7452ae0de48c
Andrei Koshmanskii <andrew_21_12@mail.ru>
2015-11-21 20:54:10 +0300

    added footer

--- MMCS_Survey.py
+++ MMCS_Survey.py
@@ -5,7 +5,7 @@ from flask.ext.mongoengine import MongoEngine
 app = Flask(__name__)
 
 app.config["MONGODB_SETTINGS"] = {'DB': "my_tumble_log"}
-app.config["SECRET_KEY"] = "KeepThisS3cr3t"
+app.config["SECRET_KEY"] = ""
 
 db = MongoEngine(app)
 
