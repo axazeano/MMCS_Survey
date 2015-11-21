eba02c24514a189835a1c176560dd906122ae376
Andrei Koshmanskii <andrew_21_12@mail.ru>
2015-11-21 23:56:27 +0300

    reordered files

---
+++ MMCS_Survey.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template
+from flask.ext.mongoengine import MongoEngine
+
+app = Flask(__name__)
+
+
+app.config["MONGODB_SETTINGS"] = {'DB': "MMCS_survey"}
+app.config["SECRET_KEY"] = ""
+
+db = MongoEngine(app)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ app/static/js/main.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $(".vert-centered").css({top: window.innerHeight / 2, left: window.innerWidth / 2, position: 'absolute'});
+});
